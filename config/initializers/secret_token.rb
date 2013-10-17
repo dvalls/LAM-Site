8c21658d0f2baad9bca346c186a4f5d73625177b
dvalls <dvalls@gmail.com>
2013-10-17 16:46:56 -0300

    start!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LAMSite::Application.config.secret_key_base = '26d52af0cd21dd4b2ace8cca33ae195f4467e9ef2c75ff48db7e9bc33e6c619ccb1eacbe91b822fc19e7ea3ddd555b42a179bf0d106414e3807a8be86174dee3'
