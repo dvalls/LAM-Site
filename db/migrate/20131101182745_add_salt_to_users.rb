6637a8e628556fa5abee856373d2f2b58e3339e8
Diogo Valls <dvalls@gmail.com>
2013-11-07 23:19:34 -0200

    cleaning old files

--- db/migrate/20131101182745_add_salt_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddSaltToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :salt, :string, limit: 128
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -156,34 +156,6 @@ ActiveRecord::Schema.define(version: 20131102152316) do
 
   add_index "projects", ["slug"], name: "index_projects_on_slug", unique: true
 
-  create_table "tag_translations", force: true do |t|
-    t.integer  "tag_id",     null: false
-    t.string   "locale",     null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "name"
-  end
-
-  add_index "tag_translations", ["locale"], name: "index_tag_translations_on_locale"
-  add_index "tag_translations", ["tag_id"], name: "index_tag_translations_on_tag_id"
-
-  create_table "taggings", force: true do |t|
-    t.integer  "tag_id"
-    t.integer  "taggable_id"
-    t.string   "taggable_type"
-    t.integer  "tagger_id"
-    t.string   "tagger_type"
-    t.string   "context",       limit: 128
-    t.datetime "created_at"
-  end
-
-  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
-
-  create_table "tags", force: true do |t|
-    t.string "name"
-  end
-
   create_table "users", force: true do |t|
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
@@ -191,7 +163,6 @@ ActiveRecord::Schema.define(version: 20131102152316) do
     t.string   "encrypted_password", limit: 128, null: false
     t.string   "confirmation_token", limit: 128
     t.string   "remember_token",     limit: 128, null: false
-    t.string   "salt",               limit: 128
   end
 
   add_index "users", ["email"], name: "index_users_on_email"
