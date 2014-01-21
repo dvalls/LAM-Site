6e1e470509ed3fc5146f9ba7fe7caefc2bb43196
dvalls <dvalls@gmail.com>
2014-01-21 18:15:37 -0200

    improving

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,11 +22,6 @@
 // require_tree .
 
 
-// Change cursor on Turbolinks on DOM transitions
-//document.addEventListener("page:fetch", $('selector').css( 'cursor', 'progress' ));
-//document.addEventListener("page:receive", $('selector').css( 'cursor', 'default' ));
-
-
 // Cleanup + enable Bootstrap tooltips on jQuery's ready event as well as
 // Turbolinks's page change event.
 $(document).on("ready page:change", function() {
