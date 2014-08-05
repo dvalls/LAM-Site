c2de0fa8e92b873b9e3a833ca07a2703ac854a3f
Diogo Valls <dvalls@gmail.com>
2014-08-04 22:59:27 -0300

    projects index

--- app/assets/javascripts/mixitup.js
+++ app/assets/javascripts/mixitup.js
@@ -1,5 +1,5 @@
 $(function(){
 
-  $('#Grid').mixitup();
+  $('#Grid').mixItUp();
 
 });
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,6 +16,7 @@ class ProjectsController < ApplicationController
 
 
   def show
+    render layout: 'project'
   end
 
 
