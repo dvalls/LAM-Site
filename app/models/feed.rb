8b30e80cd0cb39cf4a7280f47f71c0ad9ff1d06a
dvalls <dvalls@gmail.com>
2014-08-12 15:14:52 -0300

    final2

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,18 +1,6 @@
 class Feed < ActiveRecord::Base
 
-  #before_save :update_image_attributes
-
   translates :title, :body
   mount_uploader :image, FeedImageUploader
 
-  private
-
-  # TODO Avaliar necessidade disto
-  # def update_image_attributes
-  #   if image.present?
-  #     #self.content_type = image.file.content_type
-  #     self.file_size = image.file.size
-  #     self.width, self.height = `identify -format "%wx%h" #{image.file.path}`.split(/x/)
-  #   end
-  # end
 end
